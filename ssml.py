7e100ed9c66c8f024fe209d76baf76499414b6ff
duvu <hoaivubk@gmail.com>
2023-04-30 20:10:50 +0700

    refactor update version

--- ssml.py
+++ ssml.py
@@ -13,8 +13,7 @@ def build_ssml(text):
     # Create a "say-as" element for each sentence
     for sentence in sentences:
         # Split the sentence into phrases using commas
-        phrases = sentence.split(',')
-
+        phrases = sentence.split(', ')
         # Create a "say-as" element for each phrase
         for i, phrase in enumerate(phrases):
             # Check if the phrase contains a date(ddMMyyyy) or time (hh:mm)
@@ -36,6 +35,6 @@ def build_ssml(text):
 
 
 if __name__ == '__main__':
-    text = 'Trong đơn kiến nghị, Chủ tịch Tập đoàn đầu tư Địa ốc Nova (Novaland) Bùi Thành Nhơn cho biết tại Đồng Nai, các công ty thuộc Novaland là chủ đầu tư của 9 dự án. Đây là thành phần của Khu đô thị kinh tế mở Long Hưng (gồm khu dân cư Long Hưng, khu đô thị WaterFront, Aqua City) và dự án khu đô thị dịch vụ thương mại cao cấp cù lao Phước Hưng. Loạt dự án này đều thuộc phân khu C4, thành phố Biên Hòa, có nguồn gốc từ việc tách dự án hoặc Novaland nhận chuyển nhượng một phần dự án từ các chủ đầu tư cấp một.'
+    text = 'Theo tờ The Sun, Hải quân Hoàng gia Anh đã mở một cuộc điều tra về vụ sơ đồ của tàu ngầm HMS Anson trị giá 1.6 tỷ USD bị thất lạc nơi công cộng.'
     ssml = build_ssml(text)
     print(ssml)
